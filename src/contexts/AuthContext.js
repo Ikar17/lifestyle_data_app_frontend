809c74f5a9a061ea2179099c02a028c3ee5a1847
Przemek <ikaria@poczta.onet.pl>
2025-01-07 22:48:02 +0100

    add interceptor

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,6 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { auth } from '../utils/firebaseConfig';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, onIdTokenChanged, signOut} from 'firebase/auth';
 import { BACKEND_URL } from '../constants/constants';
 import axios from 'axios';
 
@@ -9,6 +9,24 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token") || "")
   const [role, setRole] = useState(localStorage.getItem("role") || "USER")
+
+  useEffect(() => {
+    // Ustawienie interceptora na starcie
+    const interceptor = axios.interceptors.response.use(
+      response => response,  // Przechwytywanie udanej odpowiedzi
+      error => { // Przechwytywanie błędów odpowiedzi
+        if (error.response && error.response.status === 403) {
+          logout();
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    // Usunięcie interceptora po odmontowaniu komponentu
+    return () => {
+      axios.interceptors.response.eject(interceptor);
+    };
+  }, []);
 
   const login = async (email, password) => {
     try{
@@ -35,7 +53,8 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  const logout = () => {
+  const logout = async () => {
+    await signOut(auth);
     setToken("");
     localStorage.removeItem('token');
     localStorage.removeItem('role');
